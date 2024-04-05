bceb92c25c68dd6893fa36c0ced57a169ee88e2d
SaqibSattar <saqibkhanz1234@gmail.com>
2024-04-05 15:46:57 +0400

    file colocation

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+const BarChart = () => {
+  return (
+    <div>Bar Chart</div>
+  )
+}
+
+const Dashboard = () => {
+    return (
+      <div>Dashboard</div>
+    )
+  }
+
+export default Dashboard
