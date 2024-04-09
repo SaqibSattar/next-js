1bcc58897b3acf16bd23cfb524a93e5714f8efa1
SaqibSattar <saqibkhanz1234@gmail.com>
2024-04-09 12:49:23 +0400

    middleware: conditional statement

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,20 @@
 import { NextResponse, type NextRequest } from "next/server";
 
+// export function middleware(request: NextRequest) {
+//   return NextResponse.redirect(new URL("/", request.url));
+// }
+
+// export const config = {
+//   matcher: "/profile",
+// };
+
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+  // redirection
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.redirect(new URL("/hello", request.nextUrl));
+  }
+  // url rewrite
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.rewrite(new URL("/hello", request.nextUrl));
+  }
 }
-
-export const config = {
-  matcher: "/profile",
-};
-
